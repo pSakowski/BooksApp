6759bdc9ce02eacc101a3c926cc3334ac990770c
psakowski <p.sakowski1213@gmail.com>
2022-09-13 21:06:07 +0200

    add function initActions

--- src/js/script.js
+++ src/js/script.js
@@ -31,4 +31,35 @@
     }
   };
   render();
+
+  const favoriteBooks = [];
+
+  function initActions() {
+    const bookList = document.querySelectorAll('.books-list .book__image');
+
+    for (let book of bookList) {
+
+      // addEventListener to bookList
+      book.addEventListener('dblclick', function(event) {
+        event.preventDefault();
+
+        // get book attribute from data-id
+        const dataId = book.getAttribute('data-id');
+
+        // remove book from favoriteBook
+        if(book.getAttribute('class').includes('favorite')) {
+          book.classList.remove('favorite');
+          
+          const index = favoriteBooks.indexOf(dataId);
+          favoriteBooks.splice(index, 1);
+        } else if (!book.getAttribute('class').includes('favorite')) {
+          book.classList.add('favorite');
+
+          // add book to favoriteBooks
+          favoriteBooks.push(dataId);
+        }
+      });
+    }
+  }
+  initActions();
 }
